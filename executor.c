fe63c70b0ecdf1266571f0b8b0b2dc65d0db01fb
AWinnerIsYou <seijiyawata@gmail.com>
2016-11-18 11:42:27 -0500

    Add files

---
+++ executor.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int main() {
+      
+  char input[100];
+  printf("Prompt: ");
+  fgets(input, sizeof(input), stdin);
+  input[strlen(input)-1] = 0; //remove newline
+
+  //printf("%s\n", input);
+  
+  //char input[] = "ls -l\n";
+
+  char * command[100];
+  char *s = input;
+  char *p;
+
+  int i = 0;
+  while (s) {
+    command[i] = strsep(&s, " ");
+    i++;
+  }
+  command[i] = 0;
+  
+  /*
+  int q = 0;
+  while (command[q]) {
+    printf("++%s\n", command[q]);
+    q++;
+  }
+  */
+  
+  execvp(command[0], command);
+  return 0;
+}
